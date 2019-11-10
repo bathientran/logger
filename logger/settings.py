801d60d30684d42662cc91fa5abf7d76a6f60fb6
Ba Thien Tran <bathientran@gmail.com>
2019-11-10 15:32:53 +0900

    includes heroku

--- logger/settings.py
+++ logger/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'j@#=8%l87)wu5=i=nhx6vh52(4%t5w$=o0+_qq2pe#!-els6kk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
